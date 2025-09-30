4de8c3ca70d66ed339bd24fe1892d0401bf9c1be
DhelipK <dhelip.programmer@gmail.com>
2025-09-30 17:23:40 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,15 @@
+# Creating directory
+mkdir -p src
+mkdir -p research
+
+# Creating files
+touch .env
+touch requirements.py
+touch setup.py
+touch app.py
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch research/trails.ipynb
+
+echo "Directory and files created successfully"
